c93797ea1e1f8e45d42237bfb0a39266c9f2ab3a
gleams <gleams@qq.com>
2021-02-22 22:35:12 +0800

    add note

--- Week 08/server.js
+++ Week 08/server.js
@@ -6,10 +6,10 @@ http.createServer((request,response)=>{
    }).on('data',(chunk)=>{
        body.push(chunk.toString());
    }).on('end',()=>{
-       body = Buffer.concat(body).toString();
+       body = Buffer.concat([Buffer.from(body.toString())]).toString();
        console.log('body:',body);
        response.writeHead(200,{'Content-Type':'text/html'})
-       response.end('Hellow world!');
+       response.end('Hello world!');
    });
 }).listen(8088);
 console.log('server started')
